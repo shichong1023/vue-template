2696ca1fdef073159c24a006fc060aadf442dac2
shichong1023 <shichong1023@163.com>
2018-10-25 11:10:09 +0800

    配置全局变量

--- template/build/utils.js
+++ template/build/utils.js
@@ -29,6 +29,13 @@ exports.cssLoaders = function (options) {
     }
   }
 
+  const sassLoader = {
+    loader: 'sass-loader',
+    options: {
+      sourceMap: options.sourceMap
+    }
+  }
+
   const px2remLoader = {
     loader: 'px2rem-loader',
     options: {
@@ -62,13 +69,45 @@ exports.cssLoaders = function (options) {
     }
   }
 
+  function resolveResouce(name) {
+    return path.resolve(__dirname, '../src/style/base/' + name);
+  }
+
+  function generateSassResourceLoader(loader, loaderOptions) {
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader, sassLoader] : [cssLoader, px2remLoader, sassLoader]
+
+    if (loader) {
+      loaders.push({
+        loader: loader + '-loader',
+        options: Object.assign({}, loaderOptions, {
+          sourceMap: options.sourceMap,
+          resources: [resolveResouce('_variable.scss'), resolveResouce('_mixins.scss')]
+        })
+      })
+    }
+
+    // Extract CSS when that option is specified
+    // (which is the case during production build)
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        use: loaders,
+        fallback: 'vue-style-loader',
+        publicPath: '../../'
+      })
+    } else {
+      return ['vue-style-loader'].concat(loaders)
+    }
+  }
+
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
     css: generateLoaders(),
     postcss: generateLoaders(),
     less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
+    // sass: generateLoaders('sass', { indentedSyntax: true }),
+    // scss: generateLoaders('sass'),
+    sass: generateSassResourceLoader('sass-resources', { indentedSyntax: true }),
+    scss: generateSassResourceLoader('sass-resources'),
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
   }
