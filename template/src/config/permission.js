7309dd1d783e3a4f0221a6e94d025a474ac73ce0
shichong1023 <shichong1023@163.com>
2018-12-03 16:05:54 +0800

    Update permission.js

--- template/src/config/permission.js
+++ template/src/config/permission.js
@@ -22,8 +22,59 @@ Vue.prototype.$_has = function (value) {
     return isExist;
   }
 
-  if (permissionStr.indexOf(value) > -1) {
-    isExist = true;
+  if (value.indexOf("||") > -1) {
+    let valArr = value.split("||")
+    for (let i = 0; i < valArr.length; ++i) {
+      if (permissionStr.indexOf(valArr[i]) > -1) {
+        isExist = true
+        break
+      }
+    }
+
+  } else {
+    if (permissionStr.indexOf(value) > -1) {
+      isExist = true
+    }
   }
+
   return isExist;
+};
+
+Vue.directive('!has', {
+  bind: function (el, binding) {
+    if (!Vue.prototype.$_nhas(binding.value)) {
+      if (el.parentNode) {
+        el.parentNode.removeChild(el);
+      } else {
+        el.style.display = "none"
+      }
+    }
+  }
+});
+
+
+Vue.prototype.$_nhas = function (value) {
+  let isExist = false;
+  let permissionStr = sessionStorage.getItem(config.proName + "_permission")
+
+  if (permissionStr == undefined || permissionStr == null) {
+    return isExist;
+  }
+
+  if (value.indexOf("||") > -1) {
+    let valArr = value.split("||")
+    for (let i = 0; i < valArr.length; ++i) {
+      if (permissionStr.indexOf(valArr[i]) > -1) {
+        isExist = true
+        break
+      }
+    }
+
+  } else {
+    if (permissionStr.indexOf(value) > -1) {
+      isExist = true
+    }
+  }
+
+  return !isExist;
 };
